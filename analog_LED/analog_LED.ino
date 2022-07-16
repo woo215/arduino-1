f442f99f8a166c097f4588a6c70cb57406510895
woo215 <108974242+woo215@users.noreply.github.com>
2022-07-16 18:11:56 +0900

    LED 아날로그 blink

--- analog_LED/analog_LED.ino
+++ analog_LED/analog_LED.ino
@@ -3,5 +3,12 @@ void setup() {
 }
 
 void loop() {
-  
+  for(int i=0; i<256; i++){
+    analogWrite(3, i);
+    delay(10);
+  }
+  for(int i=255; i>0; i--){
+    analogWrite(3, i);
+    delay(10);
+  }
 }
