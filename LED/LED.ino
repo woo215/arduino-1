dedeb958dd94b9c744e98746272dc5b548acdafc
woo215 <108974242+woo215@users.noreply.github.com>
2022-07-16 17:47:18 +0900

    LED 3blink

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,11 +1,17 @@
 void setup() {
   pinMode(7, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 void loop() {
   digitalWrite(7, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(11, HIGH);
   delay(500);
   
   digitalWrite(7, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(11, LOW);
   delay(500);
 }
