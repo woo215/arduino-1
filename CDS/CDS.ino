6fa8aa29ca8c76ba538a865aa098cf19a1ee5c5e
woo215 <108974242+woo215@users.noreply.github.com>
2022-07-16 18:50:22 +0900

    CDS 센서

--- CDS/CDS.ino
+++ CDS/CDS.ino
@@ -1,9 +1,18 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int lightValue = analogRead(A0);
+  Serial.println(lightValue);
 
+  if(lightValue < 200) {
+    digitalWrite(7, HIGH);
+  }
+  else {
+    digitalWrite(7, LOW);
+  }
+
+  delay(1000);
 }
