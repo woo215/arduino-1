6294831844a069cf7439d7f9c27e3eaddf7ebb90
woo215 <108974242+woo215@users.noreply.github.com>
2022-07-23 18:17:42 +0900

    button 코드 중

--- button/button.ino
+++ button/button.ino
@@ -1,7 +1,20 @@
 void setup() {
-  
+  Serial.begin(9600);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
 void loop() {
+  int buttonValue = digitalRead(4);
   
+  if(buttonValue==HIGH){
+  digitalWrite(13, HIGH);
+  digitalWrite(7, HIGH);
+  }
+  
+  else {
+  digitalWrite(13, LOW);
+  digitalWrite(7, LOW);
+  }
 }
