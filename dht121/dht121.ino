390d30af2078b5c055a25b3e8310e938d669c142
woo215 <108974242+woo215@users.noreply.github.com>
2022-07-30 15:54:02 +0900

    DHT 코드 ing..

--- dht121/dht121.ino
+++ dht121/dht121.ino
@@ -1,7 +1,21 @@
+#include<DHT.h>
+
+#define DHTPIN A1
+#define DHTTYPE DHT11
+DHT dnt(DHTPIN, DHTTYPE);
+
 void setup() {
-  
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  int h = DHT.readHumidity();
+  int t = DHT.readTemperature();
+
+  Serial.print("Humidity : ");
+  Serial.println(h);
+  Serial.print("Temperature : ");
+  Serial.println(t);
+
+  delay(1000);
 }
